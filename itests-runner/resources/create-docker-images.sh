7dedb01faa4ac6d793ae325297c050b093e6e0cf
tyacbovi <tyacbovi@gmail.com>
2019-11-21 18:09:50 +0200

    s3

--- itests-runner/resources/create-docker-images.sh
+++ itests-runner/resources/create-docker-images.sh
@@ -7,14 +7,14 @@ set +e
 #rm -f /tmp/docl-image-downloaded
 
 echo "# Checking whether docl image can be downloaded directly from build machine.."
-curl -I http://${DOCL_IMAGE_BUILDER_SERVER}/docl_images/docl-manager.tar.gz | grep 200
+curl -I http://cloudify-tests-files.s3.amazonaws.com/docl-images/docl-manager.tar.gz | grep 200
 
 exit_code="$?"
 if [ "$exit_code" -eq "0" ]; then
     echo "# Downloading docl image from build machine!.."
     touch /tmp/docl-image-downloaded
     set -e
-    curl -O http://${DOCL_IMAGE_BUILDER_SERVER}/docl_images/docl-manager.tar.gz
+    curl -O http://cloudify-tests-files.s3.amazonaws.com/docl-images/docl-manager.tar.gz
     echo "# Loading docl image.."
     docker -H 172.20.0.1 load -q -i docl-manager.tar.gz
 else
