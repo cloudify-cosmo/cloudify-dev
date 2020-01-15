05ca5873df99a5875abbb3f6cd8dd6900686c929
earthmant <trammell@cloudify.co>
2020-01-15 15:59:03 +0200

    report what we put in the requirements.txt

--- itests-runner/resources/create-clap-requirements.py
+++ itests-runner/resources/create-clap-requirements.py
@@ -4,11 +4,13 @@ import json
 with open('/tmp/config.json', 'r') as f:
     config = json.load(f)
 
-print "#Using this config: {0}".format(config)
-
 repositories = config['repositories']
 requirements = '\n'.join('{0}@{1}'.format(key, value)
                          for key, value in repositories.iteritems())
 
 with open('/tmp/clap-requirements.txt', 'w') as f:
     f.write(requirements)
+
+print "#Using this requirements: {0}".format(
+    open('/tmp/clap-requirements.txt', 'r').readlines()
+)
